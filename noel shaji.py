284d12e7be28b373df9052760f0fc79064f762c8
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-04 16:11:19 +0530

    Add files via upload

---
+++ noel shaji.py
@@ -0,0 +1,61 @@
+'''
+print("hello world")
+print("HELLO\nWORLD")
+if 5 > 2:
+    print("five is greater than two")
+if 5>2:
+    print("five is greater than two")
+if 5>2:
+    print("five is greater than two")
+    print("five is greater than two")
+    x=17
+    y="noel shaji"
+    print(x)
+    print(y)
+x=5
+x="thomas"
+print(x)
+first_name="jane"
+second_name="doe"
+name=first_name+" "+second_name
+print(name)
+x=int(3)
+y=float(4.7)
+z=float(3.1)
+print(type(x))
+print(type(y))
+print(type(z))
+x, y, z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+print(x, y, z)
+x="python"
+y=" is the"
+z=" most easiest language"
+
+print(x+y+z)
+
+'''
+
+'''
+username=input ("enter username")
+print("username is" + username)
+'''
+'''
+a="hello"
+b="world"
+c=a+" "+b
+print(c)
+'''
+
+
+
+
+
+
+
+
+
+
+
