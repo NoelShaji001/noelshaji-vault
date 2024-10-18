d52a9fa2070714b571f85d817fd3e76031c1ff25
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-18 15:12:04 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,15 @@
+"""
+Author:Noel Shaji
+Date:18-10-2024
+Description:Program that stores a string in a variable.Extract a specific part of the string(substring)
+and then concatenate it with another string.Display the new string.
+"""
+str_var="Hello, World!"
+str_ano_var=str_var[7:12]
+print(str_ano_var)
+str_alt_var=" Everyone!"
+var_str=str_ano_var+str_alt_var
+print(var_str)
+
+
+
