bd6634257ef1453d3364731416b7bbe3a2adfb43
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-18 14:30:05 +0530

    Add files via upload

---
+++ basicpythonperfomance.py
@@ -0,0 +1,17 @@
+"""
+Author:Noel Shaji
+Date  :18-10-2024
+Description:Program that asks user to input their name age.After receiving the input,display
+a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+"""
+name=input("Enter Your Name:")
+age=input("Enter Your Age:")
+print("Hello,",name+"! You are",age,"years old")
+
+
+
+
+
+
+
