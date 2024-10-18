b0e9b33d82f44784fd8fad75f2662ce7e62ea2a0
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-18 16:17:01 +0530

    Add files via upload

---
+++ mathmoduledemo.py
@@ -0,0 +1,13 @@
+"""
+Author: Noel Shaji
+Date:18-10-2024
+Description:Python program that uses the functions from the math module to perform the following
+operations on a number provided by the user.
+"""
+import math
+number=int(input("Enter a number"))
+print("Square root of", number,"is:",math.sqrt(number))
+print("factorial of",number,":",math.factorial(number))
+print(number,"raised to the power 2:",math.pow(number,2))
+
+
