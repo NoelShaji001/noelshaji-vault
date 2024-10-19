5753cf9cc29a599a39a6e4152199d353cd1ab7ea
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-19 14:45:06 +0530

    Add files via upload

---
+++ simplecalculatordemo.py
@@ -0,0 +1,16 @@
+"""
+Author:Noel Shaji
+Date:19-10-2024
+Description:Write a Python program that performs the following tasks.
+"""
+num1=(int(input("Enter your first number")))
+num2=(int(input("Enter your second number")))
+num3=(int(input("Enter your third number")))
+print("the sum of",num1,"and",num2,"is:",num1+num2)
+print("The difference when" ,num2, "is subtracted from" ,num1, "is:",num1-num2)
+print('The product of' ,num1, "and" ,num2, "is:",num1*num2)
+print("The quotient when" ,num1, "is divided by" ,num2,"is:",num1/num2)
+print( "The remainder when",num1,"is divided by",num2,"is:",num1%num2)
+result=(num1+num2)*num3/2
+print("The result of (",num1,"+",num2,")","*",num3,"/2 is:",result)
+
