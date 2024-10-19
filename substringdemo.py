514fdc55cf555786fc8cc6ce2a3a2d207f1d640e
NoelShaji001 <noelshaji257202@gmail.com>
2024-10-19 15:25:16 +0530

    Add files via upload

---
+++ substringdemo.py
@@ -0,0 +1,12 @@
+str_1=(input("Enter your first  name"))
+str_2=(input("Enter your last name"))
+str_3=(str_1+" "+str_2)
+print(str_3)
+str_1_length=len(str_1)
+print(str_1_length)
+sub_str_2=str_3[len(str_1)+1:]
+print(sub_str_2)
+
+
+
+
