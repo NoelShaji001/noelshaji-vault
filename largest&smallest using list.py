8db3efba4129b25876a04f1083a3001a96763e63
NoelShaji001 <noelshaji257202@gmail.com>
2024-11-17 20:40:18 +0530

    Update largest&smallest using list.py

--- largest&smallest using list.py
+++ largest&smallest using list.py
@@ -1,1 +1,13 @@
+n=int(input("enter the no. of values: "))
+numbers=[]
+for i in range(n):
+	i=int(input("enter the number"))
+	numbers.append(i)
+	i+=1
+numbers.sort(reverse=False)
+print(numbers)
+print(f"The smallest number is: {numbers[0:1]}")
+num1=len(numbers)
+print(f"The largest number is: {numbers[num1-1:]}")
+
 
